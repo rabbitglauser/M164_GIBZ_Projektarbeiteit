05517810f7a7dce14b328ab0ef4a93efd19edd65
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2025-06-16 21:33:30 +0200

    improved stuff

--- scripts/backup_restore.sh
+++ scripts/backup_restore.sh
@@ -6,9 +6,9 @@ DBNAME=$2
 FILENAME=$3
 
 if [ "$ACTION" = "backup" ]; then
-    mysqldump -u your_db_user -p $DBNAME > $FILENAME
+    mysqldump -u Root -p $DBNAME > $FILENAME
 elif [ "$ACTION" = "restore" ]; then
-    mysql -u your_db_user -p $DBNAME < $FILENAME
+    mysql -u Root -p $DBNAME < $FILENAME
 else
     echo "Usage: $0 backup|restore dbname filename.sql"
 fi
